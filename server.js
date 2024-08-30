7710522fe889754983673d9d0bd2b2df05e4466b
Shubhijain17 <17shubhijain@gmail.com>
2024-08-30 15:20:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/get', (req, res) => {
+    res.send({ message: "hello this is CICd server" });
+});
+
+app.get('/api/get_users', (req, res) => {
+    const users = [
+        { id: 1, name: "shubhi", age: 22, contact: 12345678, address: "bhopal", role: "Backend Developer" },
+    ];
+    const role=[
+        { id: 1, name: "shubhi", age: 22, contact: 12345678, address: "bhopal", role: "Backend Developer" },
+    ]
+    
+    res.send({users, role,});
+});
+
+app.listen(8000, (err) => {
+    if (err) {
+        console.error("Error starting server:", err);
+    } else {
+        console.log('Server is running on port 8000 🐿');
+    }
+});
